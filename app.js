41a7bc4d5de1535033466e0427bac950ee08eccd
Kararakt <xom-vlaskin0@outlook.com>
2023-12-07 16:57:59 +0300

    feat: added helmet

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
-const { PORT = 3000 } = process.env;
+const {
+  PORT = 3000,
+  DB_URL = 'mongodb://127.0.0.1:27017/mestodb',
+} = process.env;
 
 const app = express();
+
+app.use(helmet());
 
 app.use((req, res, next) => {
   req.user = {
@@ -14,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
